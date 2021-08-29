7dc5038196cb4ca113b54c5ec598ec7886ce507d
Mariia Zaiets <mariia_zaiets@epam.com>
2021-08-30 02:02:14 +0300

    Add settings for heroku

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
 const express = require('express')
 const path = require('path')
 const app = express()
 
-app.use(express.static(__dirname + '/dist/eternal-luck'))
+app.use(express.static(__dirname + '/dist/steam'))
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/eternal-luck/index.html'))
+  res.sendFile(path.join(__dirname + '/dist/steam/index.html'))
 })
 
 app.listen(process.env.PORT || 8080)
