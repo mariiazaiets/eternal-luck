6da348644f89d1d62ef1c2c11b5a80e0efcc54b2
Mariia Zaiets <mariia_zaiets@epam.com>
2021-08-30 12:47:02 +0300

    Change settings in server.js

--- server.js
+++ server.js
@@ -10,10 +10,11 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.use(express.static(__dirname + '/dist/steam'))
+app.use(express.static(__dirname + './dist/steam'))
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/steam/index.html'))
+app.get('*', function (req, res) {
+  console.log("redirecting to html");
+  res.sendFile(path.join(__dirname + './dist/steam/index.html'))
 })
 
 app.listen(process.env.PORT || 8080)
